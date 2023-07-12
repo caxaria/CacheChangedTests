986f18df62d4199d59a36a44af8436169bc7e0c3
Joao Caxaria <joao@codacy.com>
2023-07-12 10:01:24 +0100

    auto: updated code-25425.js

---
+++ js/code-25425.js
@@ -0,0 +1,30 @@
+//new cache content 25425
+//revision 1
+
+function a() {
+  console.log("asd" + 'dsa');
+}
+
+
+
+if(1 == "2" && "3" == 5) {
+  return;
+}
+
+var cenas = "asd132";
+
+var cemas = '123asd';
+a();
+
+b();
+
+if(true) d();
+
+var outrascenas = '123asd' + "321";
+var as= "321";
+
+
+
+var asd = '123asd' + "321";
+
+
